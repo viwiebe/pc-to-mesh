e115fae164107b269ba18ac8243b4d08b57c0af1
Viktoria Wiebe <vwiebe@uni-osnabrueck.de>
2025-10-20 04:09:09 +0200

    Changes

--- o3d_pc_to_mesh.py
+++ o3d_pc_to_mesh.py
@@ -16,10 +16,12 @@ import Imath
 import tetgen
 
 
-pc_name = ""
+pc_name = "pc"
+output_path = "results"
 original_pc = None
 used_pc = None
 debug = False
+visualize = False
 
 # default values
 voxel_size_value = 0.2
@@ -29,9 +31,6 @@ std_ratio_value = 2.0
 
 nb_points_value = 6.0
 radius_value = 1.0
-
-# default alpha shape alpha values
-alphas = [0.03]
 
 # poisson values
 octree_depth = 9
@@ -43,21 +42,23 @@ z_max = 1
 
 
 def main():
-    global pc_name, original_pc, used_pc, debug
-    global voxel_size_value, nb_neighbors_value, std_ratio_value, nb_points_value, radius_value, use_tetgen
+    global pc_name, output_path, original_pc, used_pc, debug, visualize
+    global voxel_size_value, nb_neighbors_value, std_ratio_value, nb_points_value, radius_value, use_tetgen, octree_depth
     global z_min, z_max
 
     # usage: python SCRIPTNAME [--options] "PATH/TO/CLOUD" "PATH/TO/RESULTS" 
     parser = argparse.ArgumentParser(description="Clean a point cloud for surface reconstruction as mesh for Godot.")
     parser.add_argument("input", help="Path to input point cloud")
     parser.add_argument("outpath", help="Path to output results")
-    parser.add_argument("--normals", action="store_true", help="Print dimension names to find normals")
+    parser.add_argument("--dimensions", action="store_true", help="Print dimension names to find normals")
     parser.add_argument("--name", default="cleaned_pc", help="Output image filename, default = cleaned_pc")
-    # possible arguments for this: all or multiple of: downsample, outliers, mesh, poisson
-    parser.add_argument("--parts", nargs='+', default="all", help="Which functions should be executed (default=all, options=downsample, outliers, mesh, poisson, ball)")
+    # possible arguments for this: all or multiple of: downsample, outliers, mesh (= alpha), poisson, classification, densify, ball
+    parser.add_argument("--parts", nargs='+', default="all", help="Which functions should be executed (default=all, options=downsample, outliers, alpha, poisson, ball)")
     parser.add_argument("--outliers", nargs=4, help="Values for nb neighbors, std ratio, nb points and radius in that order")
     parser.add_argument("--alpha", nargs='+', default="", help="alpha shape alpha values as a list")
+    parser.add_argument("--octree", help="Octree depth for Poisson Recon")
     parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("--visualize", action="store_true", help="Executes visualization after processing")
     args = parser.parse_args()
 
     if not len(sys.argv) > 1:
@@ -67,11 +68,17 @@ def main():
     if args.debug:
         debug = True
 
+    if args.visualize:
+        visualize = True
+
     las = laspy.read(args.input)
     if las is None:
         print("Error loading image.")
         return
     
+    if args.outpath:
+        output_path = args.outpath
+
     # save las header values
     z_min = las.header.mins[2]
     z_max = las.header.maxs[2]
@@ -80,8 +87,8 @@ def main():
         print("Header min and max...")
         print(f"Min: {z_min} | Max: {z_max}")
     
-    if args.normals:
-        print("Normals...")
+    if args.dimensions:
+        print("Dimensions...")
         print(list(las.point_format.dimension_names))
         return
 
@@ -99,11 +106,9 @@ def main():
         voxel_size_value = float(voxel)
         radius_value = float(radius)
 
-    if args.alpha is not None:
-        alphas = args.alpha
-
     if "classification" in parts:
         mesh_from_classifications(las)
+        # TODO: if no other options: terminate here
 
     used_pc = to_open3d_cloud(las)
     original_pc = to_open3d_cloud(las)
@@ -114,10 +119,12 @@ def main():
     if "all" in parts or "outliers" in parts:
         used_pc, ind = remove_outliers(used_pc)
 
-    if "all" in parts or "mesh" in parts:
+    if "all" in parts or "alpha" in parts:
         alpha_shape(used_pc)
 
     if "all" in parts or "poisson" in parts:
+        if args.octree:
+            octree_depth = int(args.octree)
         poisson_recon(pcd=used_pc)
 
     if "all" in parts or "heightmap" in parts:
@@ -128,8 +135,8 @@ def main():
     if "all" in parts or "convex" in parts:
         pc_to_mesh(pcd=used_pc)
     
-    if "all" in parts or "densify" in parts:
-        densify_pc(pcd=used_pc)
+    if "all" in parts or "delaunay" in parts:
+        delaunay(pcd=used_pc)
 
     if "all" in parts or "ball" in parts:
         ball_pivoting(pcd=used_pc)
@@ -149,6 +156,7 @@ def to_open3d_cloud(las):
     points = np.vstack((las.x, las.y, las.z)).transpose()
 
     try:
+        # add your own normal keyword here if it differs from the ones used here
         print(las)
         if "NormalX" in las:
             normals = np.vstack((
@@ -164,12 +172,9 @@ def to_open3d_cloud(las):
             )).transpose()
     except (KeyError, ValueError) as e:
         print("No normals found: %s", e)
+
     # Normalize the normals if not already unit vectors
     normals = normals / np.linalg.norm(normals, axis=1)[:, None]
-
-    # except (KeyError, ValueError) as e:
-    #     print("Normals not found in LAS file: %s", e)
-    #     normals = None
 
     # Create Open3D point cloud
     pcd = o3d.geometry.PointCloud()
@@ -184,7 +189,7 @@ def to_open3d_cloud(las):
         rgb = (rgb / 65535.0).astype(np.float64)  # Normalize to [0, 1] if 16-bit
         pcd.colors = o3d.utility.Vector3dVector(rgb)
 
-    # Save to .ply or .xyz
+    # Save to .ply or .xyz -> TODO: change to obj
     if debug:
         o3d.io.write_point_cloud(f"results/{pc_name}_converted.ply", pcd)
     
@@ -213,31 +218,89 @@ def remove_outliers(pcd):
 
 
 def alpha_shape(pcd):
+    global visualize
+
     print("Create alpha shape mesh...")
     # tetra_mesh, pt_map = o3d.geometry.TetraMesh.create_from_point_cloud(pcd)
     # o3d.visualization.draw_geometries([tetra_mesh])
+
+    # Center and scale to unit-ish size
+    center = pcd.get_center()
+    pcd.translate(-center)
+
+    bounds = pcd.get_max_bound() - pcd.get_min_bound()
+    scale = 1.0 / bounds.max()  # scale so largest dimension = 1
+    pcd.scale(scale, center=(0,0,0))
+
+    nn_dists = np.asarray(pcd.compute_nearest_neighbor_distance())
+    d = np.median(nn_dists)
+    print(f"Median neighbor distance: {d}")
+
+    # Try alpha ~ 2–5×d for first tests - 2 & 3 not so good, >=5?
+    alphas = [2 * d, 3 * d, 5 * d, 8 * d, 10 * d]
+
     for alpha in alphas:
         mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
-        o3d.io.write_triangle_mesh(f"results/alpha_mesh_{pc_name}_{alpha}.ply", mesh)
-        # why compute normals after save file???
+        # o3d.io.write_triangle_mesh(f"results/alpha_mesh_{pc_name}_{alpha}.ply", mesh)
         mesh.compute_vertex_normals()
-        o3d.visualization.draw_geometries([mesh])
+        if visualize:
+            o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
+        
+        o3d.io.write_triangle_mesh(
+            f"{pc_name}_{alpha}_alpha.obj",
+            mesh,
+            write_vertex_normals=True,
+            write_vertex_colors=True
+        )
 
 
 def ball_pivoting(pcd):
     global pc_name
     
     print("Create ball pivoting...")
-    radii = [0.005, 0.01, 0.02, 0.04]
-    rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
+    factor_list=[1.5, 2.5, 5, 10]
+    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(knn=30))
+    pcd.orient_normals_consistent_tangent_plane(100)
+
+    # estimate typical spacing (nearest neighbor distance)
+    nn_dists = np.asarray(pcd.compute_nearest_neighbor_distance())
+    # robust measure: median (less sensitive to outliers)
+    d = float(np.median(nn_dists))
+    if d == 0 or np.isnan(d):
+        d = float(np.mean(nn_dists[np.isfinite(nn_dists)]))
+
+    print(f"Estimated median nearest-neighbor distance d = {d:.6f}")
+
+    # build radii from factors
+    radii = [d * f for f in factor_list]
+    print("Trying radii:", radii)
+
+    # Tried first:
+    # bbox_size = np.linalg.norm(pcd.get_max_bound() - pcd.get_min_bound())
+    # radii = [bbox_size * f for f in [0.01, 0.02, 0.05, 0.1]]
+    # pcd.estimate_normals()
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
         pcd, 
         o3d.utility.DoubleVector(radii)
     )
-    o3d.visualization.draw_geometries([pcd, rec_mesh])
+    # o3d.visualization.draw_geometries([pcd, rec_mesh])
+    
+    center = mesh.get_center()
+    mesh.translate(-center)
+
+    scale_and_clean(mesh)
+
+    # create obj mesh
+    o3d.io.write_triangle_mesh(
+        f"{pc_name}_meshbpa.obj",
+        mesh,
+        write_vertex_normals=True,
+        write_vertex_colors=True
+    )
 
 
 def poisson_recon(pcd):
-    global octree_depth, poisson_density_cutoff, pc_name
+    global octree_depth, poisson_density_cutoff, pc_name, output_path
 
     print("Run Poisson surface reconstruction...")
     with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
@@ -248,34 +311,38 @@ def poisson_recon(pcd):
         center = mesh.get_center()
         mesh.translate(-center)
 
-        # Step 2: Scale mesh to ~10 units in largest dimension
-        bounds = mesh.get_max_bound() - mesh.get_min_bound()
-        scale_factor = 10.0 / max(bounds)  # scale largest dimension to 10
-        mesh.scale(scale_factor, center=np.array([0, 0, 0]))
+        scale_and_clean(mesh)
 
-        # Rotate mesh from Z-up (Open3D) to Y-up (Godot)
-        R = mesh.get_rotation_matrix_from_xyz((-np.pi / 2, 0, 0))  # rotate -90° around X
-        mesh.rotate(R, center=np.array([0, 0, 0]))
-
-        # Clean up the mesh
-        mesh.remove_duplicated_vertices()
-        mesh.remove_duplicated_triangles()
-        mesh.remove_degenerate_triangles()
-        mesh.remove_non_manifold_edges()
-        mesh.remove_unreferenced_vertices()
-
-        # Recompute valid normals
-        mesh.compute_vertex_normals()
-
-        # o3d.io.write_triangle_mesh("reconstructed_mesh.ply", mesh)
         o3d.io.write_triangle_mesh(
-            f"{pc_name}_mesh.obj",
+            f"{output_path}/{pc_name}_poisson.obj",
             mesh,
             write_vertex_normals=True,
             write_vertex_colors=True
         )
         print("Reconstruction complete")
         return mesh
+
+
+def scale_and_clean(mesh):
+    
+    # Scale mesh to 10 units in largest dimension
+    bounds = mesh.get_max_bound() - mesh.get_min_bound()
+    scale_factor = 10.0 / max(bounds)  # scale largest dimension to 10
+    mesh.scale(scale_factor, center=np.array([0, 0, 0]))
+
+    # Rotate mesh from Z-up (Open3D) to Y-up (Godot)
+    R = mesh.get_rotation_matrix_from_xyz((-np.pi / 2, 0, 0))  # rotate -90° around X
+    mesh.rotate(R, center=np.array([0, 0, 0]))
+
+    # Clean up the mesh
+    mesh.remove_duplicated_vertices()
+    mesh.remove_duplicated_triangles()
+    mesh.remove_degenerate_triangles() # was sind die?
+    mesh.remove_non_manifold_edges() # warum das?
+    mesh.remove_unreferenced_vertices() # warum das?
+
+    # Recompute valid normals
+    mesh.compute_vertex_normals()
 
 
 def generate_heightmap(pcd, debug_run):
@@ -465,47 +532,6 @@ def pc_to_mesh(pcd):
     o3d.io.write_triangle_mesh(f"results/convex_hull_{pc_name}.ply", mesh)
 
 
-def densify_pc(pcd):
-    # (Optional) voxel downsample to regularize spacing
-    # pcd = pcd.voxel_down_sample(voxel_size=0.05)
-
-    # Estimate normals (needed for surface ops)
-    # pc.estimate_normals()
-
-    # # Step 1: Build a coarse mesh (e.g., via Poisson or Alpha Shape)
-    # mesh, _ = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pc, depth=6)
-
-    # # Step 2: Sample new points uniformly on the mesh surface
-    # pcd_dense = mesh.sample_points_uniformly(number_of_points=50000)
-
-    # points = np.asarray(pc.points)
-    # u, s, vh = np.linalg.svd(points - np.mean(points, axis=0))
-    # print("Singular values:", s)
-    # result: not near zero! Singular values: [7396.79796327 6588.11241688 1420.20828388]
-
-    # o3d.visualization.draw_geometries([pcd_dense])
-    pcd.estimate_normals()
-    # methods for trying to solve error "invalid tetra mesh"
-    #pc.remove_duplicated_points()
-    pc = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)[0]
-    alpha = 0.5
-    mesh_alpha = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pc, alpha)
-    pcd_dense = mesh_alpha.sample_points_uniformly(number_of_points=30000)
-
-    # Step 2: Resample dense points from alpha mesh
-    pcd_dense = mesh_alpha.sample_points_uniformly(number_of_points=30000)
-    pcd_dense.estimate_normals()
-
-    # Step 3: Run Ball Pivoting Meshing
-    radii = [0.05, 0.1, 0.2]  # choose based on scale of your data
-    mesh_bpa = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
-        pcd_dense,
-        o3d.utility.DoubleVector(radii)
-    )
-
-    o3d.visualization.draw_geometries([mesh_bpa])
-
-
 def delaunay(pcd):
     points = np.asarray(pcd.points)
 
@@ -525,7 +551,7 @@ def delaunay(pcd):
 
 
 def mesh_from_classifications(las):
-    global debug
+    global debug, visualize, output_path, pc_name
 
     if debug:
         print("Unique classes:", set(las.classification))
@@ -543,6 +569,8 @@ def mesh_from_classifications(las):
 
     # create ground mesh
     mesh = None
+
+    # TODO: make able to choose which method for mesh
     if len(ground_pts) > 3:
         tri = Delaunay(ground_pts[:, :2])
         mesh = o3d.geometry.TriangleMesh()
@@ -584,7 +612,35 @@ def mesh_from_classifications(las):
 
     geoms += [pcd_ground, pcd_buildings]
 
-    o3d.visualization.draw_geometries(geoms)
+    if visualize:
+        o3d.visualization.draw_geometries(geoms)
+
+    # save as obj
+    combined_mesh = o3d.geometry.TriangleMesh()
+
+    if mesh:
+        combined_mesh += mesh
+    for box_mesh in box_meshes:
+        combined_mesh += box_mesh
+
+    # Center mesh around origin and scale to manageable size
+    center = combined_mesh.get_center()
+    combined_mesh.translate(-center)
+
+    bounds = combined_mesh.get_max_bound() - combined_mesh.get_min_bound()
+    max_dim = bounds.max()
+    if max_dim > 0:
+        scale = 10.0 / max_dim  # scale so largest dimension is about 10 units
+        combined_mesh.scale(scale, center=(0, 0, 0))
+    
+    scale_and_clean(combined_mesh)
+
+    o3d.io.write_triangle_mesh(
+        f"{output_path}/{pc_name}_classification_mesh.obj",
+        combined_mesh,
+        write_vertex_normals=True,
+        write_vertex_colors=True
+    )
 
 
 def bbox_to_mesh(box, color=(1,0,0)):
